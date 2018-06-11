7e8c9a0e30a3068001b4d27e7788238605a62f13
Mamerto Fabian Jr <mamerto@codefrost.com>
2018-06-11 11:14:22 +0800

    Added fontawesome, fixed bootstrap dropdown, added shell controllers

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialCtrl.Controllers {
+    public class InventoryController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ManageController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialCtrl.Controllers {
+    public class ManageController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
